cde0b058edbadb1b845174de2b217204dff72419
tg328 <tg328@students.waikato.ac.nz>
2026-05-25 20:07:44 +1200

    Added code folder and initialised projectio project

---
+++ Software/PomodoroTimer/src/config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define SNS_ENCODER_A 2
+#define SNS_ENCODER_B 3
+#define SNS_ENCODER_BUTTON A5
+#define SNS_START_BUTTON A6
+#define SNS_RESET_BUTTON A7
+
+#define CTRL_7SEG_A A3
+#define CTRL_7SEG_B A4
+#define CTRL_7SEG_C 4
+#define CTRL_7SEG_D 5
+#define CTRL_7SEG_E 6
+#define CTRL_7SEG_F 7
+#define CTRL_7SEG_G 8
+#define CTRL_7SEG_DP 9
+
+#define CTRL_7SEG_DIG_1 10
+#define CTRL_7SEG_DIG_2 11
+#define CTRL_7SEG_DIG_3 12
+#define CTRL_7SEG_DIG_4 13
+
+#define CTRL_SPEAKER A0
+#define CTRL_LED_BLINK A1
+#define CTRL_VIBRATOR A2
+
